c546fb096063fb40c86a9d0ed555ad9c9c9d1a53
Christopher Hunter <christopher.hunter@bigfishgames.com>
2017-05-08 14:44:10 -0700

    RT 252841 php working

--- index.php
+++ index.php
@@ -10,20 +10,19 @@
     $twig_cache_folder = "$workDir/cache";
     require_once $twig_autoloader_path;
     Twig_Autoloader::register();
-
-
     $loader = new Twig_Loader_Filesystem($twig_template_path);
     $twig = new Twig_Environment($loader,
-                                 ['cache' => $twig_cache_folder]);
+                                 ['cache' => false,
+                                 #['cache' => $twig_cache_folder,
+
+                             ]);
+    #include __DIR__.'/one-mc-show/templates/index.twig';
 
 
     // get image file list
-    ob_start();
-    passthru("ls one-mc-show/images/*.jpg");
-    $indexList = ob_get_clean();
+    //$imageList = scandir("one-mc-show/images/*.jpg");
+    $imageList = glob("$workDir/one-mc-show/images/IMG*.jpg", GLOB_ERR);
 
-    // render PHP template
-    #include __DIR__.'/one-mc-show/templates/index.twig';
     // read and render Twig template
     $template = $twig->loadtemplate("index.twig");
     $pageTitle = 'One MC Show';
