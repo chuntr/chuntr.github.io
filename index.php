b08240c0c4c2c25b78527d82d4a326b7c6765431
Christopher Hunter <christopher.hunter@bigfishgames.com>
2017-05-08 12:43:23 -0700

    restructuring

--- index.php
+++ index.php
@@ -4,28 +4,30 @@
     ob_start();
     passthru("ls one-mc-show/images/*.jpg");
     $indexList = ob_get_clean();
-    #print $indexList;
 
     // setup Twig environment
-    /*
     $twig_autoloader_path = 'vendor/twig/twig/lib/Twig/Autoloader.php';
+    $twig_template_path = 'one-mc-show/templates';
+    $twig_cache_folder = 'cache';
     require_once $twig_autoloader_path;
     Twig_Autoloader::register();
-    $loader = new Twig_Loader_Filesystem($twig_autoloader_path);
-    $twig = new Twig_Environment($loader);
 
-    $escaper = new Twig_Extension_Escaper(false);
-    $twig->addExtension($escaper);
+
+    $loader = new Twig_Loader_Filesystem($twig_template_path);
+    $twig = new Twig_Environment($loader,
+                                 ['cache' => $twig_cache_folder]);
 
     // read and render Twig template
-    $template = $twig->loadtemplate('onec-mc-show/templates/index.phtml');
+    $template = $twig->loadtemplate('one-mc-show/templates/index.twig');
     $template->display($params);
-    */
+
+    $imageRoot = 'https://chunter.github.io/one-mc-show/images/';
 
     $pageTitle = 'One MC Show';
-    $params = ['indexList' => $indexList];
+    $params = ['imageRoot' => $imageRoot,
+                'indexList' => $indexList ];
 
     // render PHP template
-    include __DIR__.'/one-mc-show/templates/index.phtml';
+    include __DIR__.'/one-mc-show/templates/index.twig';
 
     echo $twig->render(params);
